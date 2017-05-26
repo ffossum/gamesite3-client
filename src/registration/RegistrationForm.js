92c3f112ded6ed97643fe7488c92716f741a6542
Fredrik Fossum <fredrik.fossum@gmail.com>
2017-05-26 20:22:18 +0200

    Add registration form component

---
+++ src/registration/RegistrationForm.js
@@ -0,0 +1,75 @@
+/* @flow */
+import React from 'react';
+
+export default class RegistrationForm extends React.Component {
+  handleSubmit: (e: SyntheticInputEvent) => void;
+  handleChange: (e: SyntheticInputEvent) => void;
+  state: {
+    username: string,
+    email: string,
+    password: string,
+    repeatPassword: string,
+  };
+  constructor() {
+    super();
+
+    this.handleSubmit = e => {
+      e.preventDefault();
+    };
+
+    this.handleChange = e => {
+      const { name, value } = e.target;
+      this.setState(() => ({
+        [name]: value,
+      }));
+    };
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Repeat password</label>
+          <input
+            type="password"
+            id="repeatPassword"
+            name="repeatPassword"
+            onChange={this.handleChange}
+          />
+        </div>
+
+        <button type="submit">Register</button>
+      </form>
+    );
+  }
+}
