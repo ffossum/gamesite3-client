d2ab518c9d5ad4e0e4c545dc25102985b0b61d92
Fredrik Fossum <fredrik.fossum@gmail.com>
2017-05-28 16:28:32 +0200

    Add tests for registration reducer

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -3,4 +3,4 @@ import type {
   RegistrationAction,
 } from './modules/registration/registrationActions';
 
-export type Action = RegistrationAction;
+export type Action = RegistrationAction | { type: '@@INIT' };
---
+++ src/redux/modules/registration/registrationReducer.test.js
@@ -0,0 +1,39 @@
+/* @flow */
+/* eslint-env jest */
+import reducer from './registrationReducer';
+import {
+  registrationRequest,
+  registrationSuccess,
+  registrationFailure,
+} from './registrationActions';
+
+describe('registration reducer', () => {
+  const registration = {
+    username: 'bob',
+    email: 'bob@test.com',
+    password: 'bobisthebest',
+    repeatPassword: 'bobisthebest',
+  };
+  const initialState = reducer(undefined, { type: '@@INIT' });
+  test('initial state', () => {
+    expect(initialState).toMatchSnapshot();
+  });
+
+  test('is loading after request', () => {
+    const action = registrationRequest(registration);
+    const state = reducer(initialState, action);
+    expect(state.loading).toBe(true);
+  });
+
+  test('is no longer loading after success', () => {
+    let state = reducer(initialState, registrationRequest(registration));
+    state = reducer(state, registrationSuccess());
+    expect(state.loading).toBe(false);
+  });
+
+  test('is no longer loading after failure', () => {
+    let state = reducer(initialState, registrationRequest(registration));
+    state = reducer(state, registrationFailure());
+    expect(state.loading).toBe(false);
+  });
+});
