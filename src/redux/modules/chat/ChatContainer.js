a704165472c22eb6b8add5557b83d73b709344ad
Fredrik Fossum <fredrik.fossum@gmail.com>
2017-06-03 14:34:17 +0200

    Render chat messages

--- src/redux/modules/chat/ChatContainer.js
+++ src/redux/modules/chat/ChatContainer.js
@@ -4,23 +4,38 @@ import { connect } from 'react-redux';
 import Chat from '../../../components/chat/Chat';
 import type { Props as ChatProps } from '../../../components/chat/Chat';
 import { sendMessage } from './chatActions';
+import type { State } from '../root';
 
 type Props = {
   channelName: string,
 };
 
 export default connect(
-  state => ({
-    user: state.session.user,
-    messages: [],
-  }),
+  (state: State, ownProps: Props) => {
+    const channel = state.chat[ownProps.channelName] || {};
+    const messages = channel.messages || [];
+
+    return {
+      user: state.session.user,
+      messages: messages.map(msg => ({
+        user: {
+          id: msg.userId,
+          username: msg.userId, // TODO
+        },
+        text: msg.text,
+      })),
+    };
+  },
   (dispatch: Dispatch<*>) => bindActionCreators({ sendMessage }, dispatch),
   (stateProps, dispatchProps, ownProps: Props): ChatProps => {
     const userId = stateProps.user && stateProps.user.id;
+    let partialSendMessage = () => {};
 
-    const partialSendMessage = messageText => {
-      dispatchProps.sendMessage(userId, ownProps.channelName, messageText);
-    };
+    if (userId) {
+      partialSendMessage = messageText => {
+        dispatchProps.sendMessage(userId, ownProps.channelName, messageText);
+      };
+    }
 
     return {
       ...stateProps,
