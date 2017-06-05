97991c4e85a1501d2aec834defd7c7b364a0bd5b
Fredrik Fossum <fredrik.fossum@gmail.com>
2017-06-05 11:29:11 +0200

    Add tests for LoginFormContainer

--- src/redux/modules/login/LoginFormContainer.js
+++ src/redux/modules/login/LoginFormContainer.js
@@ -3,15 +3,21 @@ import { connect } from 'react-redux';
 import RegistrationForm from '../../../components/login/LoginForm';
 import { loginRequest } from './loginActions';
 
+import type { Login } from './loginActions';
 import type { State } from '../root';
 
-export default connect(
-  (state: State) => ({
-    loading: state.registration.loading,
-  }),
-  (dispatch: Dispatch<*>) => ({
-    logIn(...args) {
-      dispatch(loginRequest(...args));
+export function mapStateToProps(state: State) {
+  return {
+    loading: state.login.loading,
+  };
+}
+
+export function mapDispatchToProps(dispatch: Dispatch<*>) {
+  return {
+    logIn(login: Login) {
+      dispatch(loginRequest(login));
     },
-  })
-)(RegistrationForm);
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(RegistrationForm);
---
+++ src/redux/modules/login/LoginFormContainer.test.js
@@ -0,0 +1,60 @@
+/* @flow */
+/* eslint-env jest */
+import React from 'react';
+import LoginFormContainer, {
+  mapStateToProps,
+  mapDispatchToProps,
+} from './LoginFormContainer';
+import { loginRequest } from './loginActions';
+
+import { createStore } from 'redux';
+import { rootReducer } from '../root';
+import { Provider } from 'react-redux';
+import { mount } from 'enzyme';
+import serializer from 'enzyme-to-json/serializer';
+
+expect.addSnapshotSerializer(serializer);
+
+describe('login form container', () => {
+  test('is a renderable React component', () => {
+    const store = createStore(rootReducer);
+    const rendered = mount(
+      <Provider store={store}>
+        <LoginFormContainer />
+      </Provider>
+    );
+
+    expect(rendered).toMatchSnapshot();
+  });
+
+  describe('mapStateToProps', () => {
+    test('selects loading state', () => {
+      const store = createStore(rootReducer);
+      expect(mapStateToProps(store.getState()).loading).toBe(false);
+
+      store.dispatch(
+        loginRequest({ email: 'asdf@asdf.com', password: 'asdfasdf' })
+      );
+      expect(mapStateToProps(store.getState()).loading).toBe(true);
+    });
+  });
+
+  describe('mapDispatchToProps', () => {
+    test('logIn dispatches loginRequest action', () => {
+      const dispatch = jest.fn();
+      const props = mapDispatchToProps(dispatch);
+      props.logIn({
+        email: 'qwer@qwer.com',
+        password: 'qwerqwer',
+      });
+
+      expect(dispatch).toHaveBeenCalledTimes(1);
+      expect(dispatch).toHaveBeenCalledWith(
+        loginRequest({
+          email: 'qwer@qwer.com',
+          password: 'qwerqwer',
+        })
+      );
+    });
+  });
+});
