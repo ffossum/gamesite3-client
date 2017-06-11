eda1acb98ae2de064c70a5aa6a5788eff8993126
Fredrik Fossum <fredrik.fossum@gmail.com>
2017-06-11 19:44:55 +0200

    Add timestamp to game in lobby

--- src/components/lobby/Lobby.js
+++ src/components/lobby/Lobby.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import type { ContextRouter } from 'react-router-dom';
 import type { SessionUser } from '../../redux/modules/session/sessionReducer';
-
+import format from 'date-fns/format';
 import CreateGameForm from './CreateGameForm';
 import ChatContainer from '../../redux/modules/chat/ChatContainer';
 
@@ -38,7 +38,8 @@ export default function Lobby(props: Props) {
           {games.map(game =>
             <li key={game.id}>
               <Link to={`/game/${game.id}`}>
-                Host: {game.host.username}, created: {game.createdTime}
+                Host: {game.host.username}, created:{' '}
+                {format(game.createdTime, 'ddd, MMM Do HH:mm')}
               </Link>
             </li>,
           )}
