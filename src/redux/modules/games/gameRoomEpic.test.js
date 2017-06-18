1f6a4f64170e3c3becc8277d8a52dec1a9152488
Fredrik Fossum <fredrik.fossum@gmail.com>
2017-06-19 00:01:54 +0200

    Add tests for enter/leave spectator room

--- src/redux/modules/games/gameRoomEpic.test.js
+++ src/redux/modules/games/gameRoomEpic.test.js
@@ -2,7 +2,12 @@
 /* eslint-env jest */
 import { Observable } from 'rxjs';
 import gameRoomEpic from './gameRoomEpic';
-import { joinGame, leaveGame } from './gameRoomActions';
+import {
+  joinGame,
+  leaveGame,
+  enterSpectatorRoom,
+  exitSpectatorRoom,
+} from './gameRoomActions';
 
 describe('game room epic', () => {
   let store: any;
@@ -11,7 +16,35 @@ describe('game room epic', () => {
   beforeEach(() => {
     deepstreamClient = {
       make: jest.fn(),
+      subscribe: jest.fn(),
     };
+  });
+
+  test('subscribes to spectator events when entering spectator room', async () => {
+    const action = enterSpectatorRoom('game_id');
+    const action$ = Observable.of(action);
+
+    deepstreamClient.subscribe.mockReturnValue(Observable.empty());
+
+    await gameRoomEpic(action$, store, { deepstreamClient })
+      .toArray()
+      .toPromise();
+
+    expect(deepstreamClient.subscribe).toHaveBeenCalledTimes(1);
+    expect(deepstreamClient.subscribe).toHaveBeenCalledWith('spectate:game_id');
+  });
+
+  test('unsubscribes from spectator events when exiting spectator room', async () => {
+    const action$ = Observable.of(
+      enterSpectatorRoom('game_id'),
+      exitSpectatorRoom('game_id'),
+    );
+
+    deepstreamClient.subscribe.mockReturnValue(Observable.never());
+    await gameRoomEpic(action$, store, { deepstreamClient })
+      .toArray()
+      .toPromise();
+    // Pass = No timeout
   });
 
   test('makes rpc join game request', async () => {
